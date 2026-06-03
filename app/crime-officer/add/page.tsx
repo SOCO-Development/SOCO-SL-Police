73eb844e2742f3b545d11e78f3743ad56c877716
Rasindu Bandara <samadith.r@gmail.com>
2026-06-03 11:15:30 +0530

    restored

--- app/crime-officer/add/page.tsx
+++ app/crime-officer/add/page.tsx
@@ -1010,7 +1010,7 @@ export default function AddOfficerPage() {
                                             </tbody>
                                         </table>
                                     </div>
-                                    <AddRowButton onClick={addPromotion}>Add Promotionn</AddRowButton>
+                                    <AddRowButton onClick={addPromotion}>Add Promotion</AddRowButton>
                                 </div>
                             </div>
 
